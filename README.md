ed820455c0f4f78d1c3849990ca44e491a5b0c05
suhasinipatil <suhasinibpatil09@gmail.com>
2023-02-17 22:45:53 +0530

    Added Readme and handled error response using status codes

---
+++ README.md
@@ -0,0 +1,20 @@
+# Task Manager (Spring Boot)
+
+## Development
+
+### Build and Run
+
+Without IDE, we can build and run via Gradle
+
+Run directly
+
+``
+./gradlew bootRun
+``
+
+Build jar and run
+
+``
+./gradlew bootJar
+ava -jar .\build\libs\spring-taskmgr-0.0.1-SNAPSHOT.jar
+``
--- src/main/java/com/example/springtaskmgr/controllers/TaskController.java
+++ src/main/java/com/example/springtaskmgr/controllers/TaskController.java
@@ -1,12 +1,15 @@
 package com.example.springtaskmgr.controllers;
 
+import com.example.springtaskmgr.dtos.ErrorResponse;
 import com.example.springtaskmgr.entities.Task;
 import com.example.springtaskmgr.service.TasksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -21,27 +24,37 @@ public class TaskController {
     }
 
     @GetMapping("/tasks")
-    List<Task> getTaskList(){
-        return tasksService.getTasks();
+    ResponseEntity<List<Task>> getTaskList(){
+        return ResponseEntity.ok(tasksService.getTasks());
     }
 
     @PostMapping("/tasks")
-    Task createTask(@RequestBody Task task) {
-        return tasksService.createTask(task.getTitle(), task.getDescription(), task.getDueDate());
+    ResponseEntity<Task> createTask(@RequestBody Task task) {
+        var newTask = tasksService.createTask(task.getTitle(), task.getDescription(), task.getDueDate());
+        return ResponseEntity.created(URI.create("/tasks/" + newTask.getId())).body(newTask);
     }
 
     @GetMapping("/tasks/{id}")
-    Task getTask(@PathVariable("id") Integer id){
-       return tasksService.getTaskById(id);
+    ResponseEntity<Task> getTask(@PathVariable("id") Integer id){
+       return ResponseEntity.ok(tasksService.getTaskById(id));
     }
 
     @DeleteMapping("/tasks/{id}")
-    Task deleteTask(@PathVariable("id") Integer id){
-        return tasksService.deleteTask(id);
+    ResponseEntity<Task> deleteTask(@PathVariable("id") Integer id){
+        return ResponseEntity.accepted().body(tasksService.deleteTask(id));
     }
 
     @PatchMapping("/tasks/{id}")
-    Task updateTask(@RequestBody Task task, @PathVariable("id") Integer id){
-        return tasksService.updateTask(id, task.getTitle(), task.getDescription(), task.getDueDate());
+    ResponseEntity<Task> updateTask(@RequestBody Task task, @PathVariable("id") Integer id){
+        var updatedTask = tasksService.updateTask(id, task.getTitle(), task.getDescription(), task.getDueDate());
+        return ResponseEntity.accepted().body(updatedTask);
+    }
+
+    @ExceptionHandler(TasksService.TaskNotFoundException.class)
+    ResponseEntity<ErrorResponse> handleErrors(TasksService.TaskNotFoundException e){
+        return new ResponseEntity<>(
+                new ErrorResponse(e.getMessage()),
+                HttpStatus.NOT_FOUND
+        );
     }
 }
